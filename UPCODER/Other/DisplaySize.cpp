0bdaf7f6ef1a32756524d6f58386f8796e20b6b4
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-12 22:04:11 +0700

    DisplaySize

---
+++ UPCODER/Other/DisplaySize.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define elif else if 
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+signed main() {
+    fastIO;
+    int n;
+    cin >> n;
+
+    int w = 1, l = n;
+    for (int i = 1; i*i <= n; i++) {
+        if (n % i == 0) {
+            int j = n / i;
+            if (j - i < l - w) {
+                l = j;
+                w = i;
+            }
+        }
+    }
+    
+    cout << w << ' ' << l;
+    return 0;
+}
