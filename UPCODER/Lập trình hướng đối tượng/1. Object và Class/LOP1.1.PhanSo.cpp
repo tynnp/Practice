e97f55d4c801bc9d61c3215f85265f19f619b27e
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-15 08:33:46 +0700

    LOP1.1.PhanSo

--- UPCODER/Lập trình hướng đối tượng/1. Object và Class/LOP1.1.PhanSo.cpp
+++ UPCODER/Lập trình hướng đối tượng/1. Object và Class/LOP1.1.PhanSo.cpp
@@ -1,66 +1,25 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 class PhanSo {
 private:
-    int tu;
-    int mau;
+    int tuSo, mauSo;
+
 public:
-    void setTu(int tu) {
-        this->tu = tu;
-        return;
-    }
-
-    void setMau(int mau) {
-        this->mau = mau;
-        return;
-    }
-
-    int getTu() {
-        return tu;
-    }
-
-    int getMau() {
-        return mau;
-    }
-
-    PhanSo() {}
-
-    PhanSo(int tu, int mau): tu(tu), mau(mau) {}
-
-    PhanSo(const PhanSo &p) {
-        this->tu = p.tu;
-        this->mau = p.mau;
-    } 
-
-    ~PhanSo() {}
-
-    friend istream& operator >> (istream &in, PhanSo &p) {
-        in >> p.tu >> p.mau;
-        return in;
-    }
-
-    friend ostream& operator << (ostream &out, PhanSo p) {
-        int ucln = __gcd(p.tu, p.mau);
-        p.tu /= ucln;
-        p.mau /= ucln;
-
-        if (p.mau < 0)
-            p.tu = -p.tu,
-            p.mau = -p.mau;
-
-        out << p.tu << "/" << p.mau;
-        return out;
-    }
+    void nhap();    // thao tác nhập
+    void xuat();    // thao tác xuất
 };
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
     PhanSo p;
-    cin >> p;
-    cout << p;
-    return 0;
+    p.nhap();
+    p.xuat();
+}
+
+void PhanSo::nhap() {
+    cin >> tuSo >> mauSo;
+}
+
+void PhanSo::xuat() {
+    cout << tuSo << '/' << mauSo;
 }
