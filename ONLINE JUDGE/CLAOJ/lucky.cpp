447bdec716a639f61b2259a8ca2a8144ad1328f6
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-18 02:07:25 +0700

    Số may mắn

---
+++ ONLINE JUDGE/CLAOJ/lucky.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define elif else if 
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int n, cnt = 1;
+string ans;
+queue<string> q;
+
+signed main() {
+    fastIO;
+    cin >> n;
+
+    q.push("4");
+    q.push("7");
+
+    while (cnt <= n) {
+        ans = q.front();
+        q.pop();
+
+        cnt++;
+        q.push(ans + "4");
+        q.push(ans + "7");
+    }
+
+    cout << ans;
+    return 0;
+}
