f52d07932edc7e4ab021c6cb7af1090459b34b79
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-19 01:47:58 +0700

    HSG THPT Hải Phòng 2023 - Bài 2

--- ONLINE JUDGE/VNOJ/Kỳ thi Học sinh giỏi THPT TP Hải Phòng 2021/HSG THPT Hải Phòng 2021 - Bài 1.py
+++
@@ -1,4 +0,0 @@
-s = input()
-for i in range(0, len(s)):
-    if s[i].isalpha():
-        print(s[i], end='')
--- ONLINE JUDGE/VNOJ/Kỳ thi Học sinh giỏi THPT TP Hải Phòng 2023/HSG THPT Hải Phòng 2023 - Bài 2.py
+++
@@ -1,7 +0,0 @@
-s = input()
-
-ans = 0
-for i in range(len(s)):
-    if s[i].isdigit():
-        ans += 1
-print(ans)
