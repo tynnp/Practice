5d4d8a94870493ef9fc7f8821449eb438c1e8584
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-19 10:23:28 +0700

    Đếm cặp số

---
+++ ONLINE JUDGE/LQDOJ/findpair.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define elif else if 
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int n, x, a[MAXN];
+
+signed main() {
+    fastIO;
+    cin >> n >> x;
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    int i = 1, j = n;
+    while (i <= j) {
+        if (a[i] + a[j] == x) {
+            cout << i << ' ' << j;
+            break;
+        }
+
+        if (a[i] + a[j] > x) j--;
+        else i++;
+    }
+    
+    if (i > j)
+        cout << "No solution";
+    return 0;
+}
