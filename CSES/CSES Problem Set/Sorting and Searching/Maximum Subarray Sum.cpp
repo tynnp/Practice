62ebc23b72f336f0016c34e7b8c15821e4050b6a
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-28 01:46:46 +0700

    Maximum Subarray Sum

---
+++ CSES/CSES Problem Set/Sorting and Searching/Maximum Subarray Sum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const int MAXN = 2e5 + 5;
+const int MOD = 1e9 + 7;
+
+int n, ans, sum;
+int a[MAXN];
+
+signed main() {
+    fastIO;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    ans = sum = a[1];
+    for (int i = 2; i <= n; i++) {
+        sum = max(a[i], sum + a[i]);
+        ans = max(ans, sum);
+    }
+    
+    cout << ans;
+    return 0;
+}
