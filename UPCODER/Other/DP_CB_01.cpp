15ac5101ef7bec9d2509cb9e14c7f2206482a880
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-28 01:55:10 +0700

    DP_CB_01

--- UPCODER/Other/DP_CB_01.cpp
+++ UPCODER/Other/DP_CB_01.cpp
@@ -19,20 +19,20 @@ int n, ans;
 int a[MAXN], dp[MAXN];
 
 signed main() {
-    fastIO; 
+    fastIO;
     cin >> n;
-    fill(dp, dp + n, 1);
-    
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < i; j++) 
-            if (a[j] > a[i]) 
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    fill(dp, dp + n, 1);
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j < i; j++)     
+            if (a[i] < a[j])
                 dp[i] = max(dp[i], dp[j] + 1);
         ans = max(ans, dp[i]);
     }
-
+    
     cout << ans;
     return 0;
-}
+} 
