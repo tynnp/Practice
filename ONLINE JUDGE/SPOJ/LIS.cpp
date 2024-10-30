2a6a0370aa13c86f69ec7cfba889df20d33f53de
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-30 12:05:39 +0700

    Day con tang dai nhat

---
+++ ONLINE JUDGE/SPOJ/LIS.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+const int MAXN = 3e4 + 5;
+const int MOD = 1e9 + 7;
+
+int n, a[MAXN];
+multiset<int> m;
+
+signed main() {
+    fastIO;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    for (int i = 1; i <= n; i++) {
+        auto it = m.lower_bound(a[i]);
+
+        if (it != m.end()) 
+            m.erase(it);
+
+        m.insert(a[i]);
+    }
+    
+    cout << m.size();
+    return 0;
+}
