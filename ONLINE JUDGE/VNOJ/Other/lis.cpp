eee5011305261d85b74181658b1d19f9ce5bd308
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-30 12:31:31 +0700

    Dãy con tăng dài nhất (bản khó)

--- ONLINE JUDGE/VNOJ/Other/lis.cpp
+++ ONLINE JUDGE/VNOJ/Other/lis.cpp
@@ -6,36 +6,34 @@ using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
-#define elif else if 
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
 #define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 
-const int MAXN = 30000 + 5;
+const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
-const int INF = 1e9;
 
-int n;
-int a[MAXN];
-vector<int> v;
+int n, a[MAXN];
+multiset<int> m;
 
 signed main() {
     fastIO;
-
     cin >> n;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 1; i <= n; i++)
         cin >> a[i];
 
-    for (int i = 0; i < n; i++) {
-        auto it = lower_bound(v.begin(), v.end(), a[i]);
-        if (it == v.end())  
-            v.push_back(a[i]);
-        else 
-            *it = a[i];
+    for (int i = 1; i <= n; i++) {
+        auto it = m.lower_bound(a[i]);
+
+        if (it != m.end()) 
+            m.erase(it);
+
+        m.insert(a[i]);
     }
     
-    cout << v.size();
+    cout << m.size();
     return 0;
 }
