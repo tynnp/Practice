5c5a4b5c85abaa83786a093e4e8fcff249b81c72
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-01 00:45:48 +0700

    Dãy con tăng dài nhất (bản dễ)

--- ONLINE JUDGE/VNOJ/Other/liq.cpp
+++ ONLINE JUDGE/VNOJ/Other/liq.cpp
@@ -6,35 +6,33 @@ using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
-#define elif else if 
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
 #define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 
-const int MAXN = 1000 + 5;
+const int MAXN = 1e3 + 5;
 const int MOD = 1e9 + 7;
-const int INF = 1e9;
 
-int n;
-int a[MAXN], f[MAXN];
+int n, ans;
+int a[MAXN], dp[MAXN];
 
 signed main() {
     fastIO;
     cin >> n;
-    
-    for (int i = 0; i < n; i++)
+
+    for (int i = 1; i <= n; i++)
         cin >> a[i];
-    a[n++] = 100000;
-    
-    for (int i = 0; i < n; i++) {
-        f[i] = 1;
-        for (int j = 0; j < i; j++)
+    fill(dp, dp + MAXN, 1);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i; j++)
             if (a[j] < a[i])
-                f[i] = max(f[i], f[j] + 1);
+                dp[i] = max(dp[i], dp[j] + 1);
+        ans = max(ans, dp[i]);
     }
     
-    cout << f[n-1] - 1;
+    cout << ans;
     return 0;
 }
