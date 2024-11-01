ba911e64006347bfab394b66229d99886fb890e6
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-01 08:56:59 +0700

    PS_tmp01

--- UPCODER/Other/1921_OOP_tmp01.cpp
+++ UPCODER/Other/1921_OOP_tmp01.cpp
@@ -1,103 +1,93 @@
 #include <iostream>
-#include <algorithm>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
 
 class PhanSo {
-private:    
+private:
     int tu, mau;
+
 public:
-    ~PhanSo() {}
+    int getTu();
+    int getMau();
 
-    PhanSo() {
-        tu = 0;
-        mau = 1;
-    }
+    PhanSo();
+    PhanSo(int tu, int mau);
+    PhanSo(const PhanSo &other);
 
-    PhanSo(int tu, int mau) {
-        this->tu = tu;
-        this->mau = mau;
-        if (this->mau < 0)
-            this->tu = -this->tu,
-            this->mau = -this->mau;
-    }
-
-    PhanSo(const PhanSo &other) {
-        this->tu = other.tu;
-        this->mau = other.mau;
-    }
-
-    void setTu(int tu) {
-        this->tu = tu;
-    }
-
-    int getTu() {
-        return tu;
-    }
-
-    void setMau(int mau) {
-        this->mau = mau;
-        if (this->mau < 0)
-            this->tu = -this->tu,
-            this->mau = -this->mau;
-    }
-
-    int getMau() {
-        return mau;
-    }
-
-    double getGiaTri() {
-        return (double) tu/mau;
-    }
-
-    PhanSo psRutGon() {
-        PhanSo res;
-        int ucln = __gcd(tu, mau);
-        res.tu = tu /= ucln;
-        res.mau = mau /= ucln;
-        
-        if (res.mau < 0) 
-            res.tu = -res.tu,
-            res.mau = -res.mau;
-        
-        return res;
-    }
-
-    PhanSo psNghichDao() {
-        PhanSo res;
-        res.tu = mau;
-        res.mau = tu;
-
-        if (res.mau < 0) 
-            res.tu = -res.tu,
-            res.mau = -res.mau;
-
-        return res;
-    }
-
-    void output() {
-        cout << tu << "/" << mau;
-        cout << endl;
-    }
-
-    friend istream &operator >> (istream &in, PhanSo &p) {
-        in >> p.tu >> p.mau;
-        if (p.mau < 0)
-            p.tu = -p.tu,
-            p.mau = -p.mau;
-        return in;
-    }
+    double getGiaTri();
+    PhanSo psRutGon();
+    PhanSo psNghichDao();
+    void output();
 };
 
 int main() {
-    PhanSo a, b;
-    cin >> a >> b;
-    a.output();
-    b.output();
-    cout << a.getTu() + b.getMau() << endl;
-    cout << fixed << setprecision(3);
-    cout << a.getGiaTri() << " " << b.getGiaTri() << endl;
-    (a.psRutGon()).output();
-    (b.psNghichDao().psRutGon()).output();
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+
+    PhanSo psA(a, b);
+    PhanSo psB(x, y);
+
+    psA.output();
+    psB.output();
+    cout << psA.getTu() + psB.getMau() << endl;
+    cout << psA.getGiaTri() << ' ' << psB.getGiaTri() << endl;
+    (psA.psRutGon()).output();
+    (psB.psNghichDao()).output();
+
     return 0;
-}   
+}
+
+int PhanSo::getTu() {
+    return tu;
+}
+
+int PhanSo::getMau() {
+    return mau;
+}
+
+PhanSo::PhanSo() {
+    tu = 0;
+    mau = 1;
+}
+
+PhanSo::PhanSo(int tu, int mau) {
+    if (mau < 0) {
+        tu = -tu;
+        mau = -mau;
+    }
+
+    this->tu = tu;
+    this->mau = mau;
+}
+
+PhanSo::PhanSo(const PhanSo &other) {
+    tu = other.tu;
+    mau = other.mau;
+}
+
+double PhanSo::getGiaTri() {
+    return (double) tu / mau;
+}
+
+PhanSo PhanSo::psRutGon() {
+    int ucln = __gcd(tu, mau);
+    PhanSo res;
+    res.tu = tu / ucln;
+    res.mau = mau / ucln;
+
+    if (res.mau < 0) {
+        res.tu = -res.tu;
+        res.mau = -res.mau;
+    }
+
+    return res;
+}
+
+PhanSo PhanSo::psNghichDao() {
+    return PhanSo(mau, tu).psRutGon();
+}
+
+void PhanSo::output() {
+    cout << fixed << setprecision(3) << tu << '/' << mau << endl;
+}
