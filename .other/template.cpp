91a179b028bdcd2956ced87a985bf8269c69e6be
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-05 02:45:14 +0700

    update template

--- .other/template.cpp
+++ .other/template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
@@ -10,7 +11,8 @@ using namespace __gnu_pbds;
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
