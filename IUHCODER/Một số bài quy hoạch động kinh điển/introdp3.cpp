77d6d3cc3075c7bce317944842d393a6f7f1e1c0
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-06 03:20:05 +0700

    LIS 1

--- IUHCODER/Một số bài quy hoạch động kinh điển/introdp3.cpp
+++ IUHCODER/Một số bài quy hoạch động kinh điển/introdp3.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
@@ -7,24 +11,25 @@ using namespace std;
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
-const int MAXN = 1e6 + 5;
+const int MAXN = 2e3 + 5;
 const int MOD = 1e9 + 7;
 
-int n, ans;
-int arr[MAXN];
-int dp[MAXN];
+int n, ans, a[MAXN], dp[MAXN];
 
 signed main() {
     fastIO;
     cin >> n;
-    for (int i = 1; i <= n; i++) 
-        cin >> arr[i];
-    
+    fill(dp, dp + MAXN, 1);
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
     for (int i = 1; i <= n; i++) {
-        dp[i] = 1;
         for (int j = 1; j < i; j++)
-            if (arr[j] < arr[i]) 
+            if (a[j] < a[i])
                 dp[i] = max(dp[i], dp[j] + 1);
         ans = max(ans, dp[i]);
     }
