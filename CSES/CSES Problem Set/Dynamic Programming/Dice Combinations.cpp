0ca230a55feaad5b830fe0163f5dbd35159d1222
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-08 01:14:08 +0700

    Dice Combinations

--- CSES/CSES Problem Set/Dynamic Programming/Dice Combinations.cpp
+++ CSES/CSES Problem Set/Dynamic Programming/Dice Combinations.cpp
@@ -24,11 +24,10 @@ signed main() {
     cin >> n;
 
     dp[0] = 1;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
         for (int j = 1; j <= 6; j++)
             if (i - j >= 0)
                 (dp[i] += dp[i - j]) %= MOD;
-    }
     
     cout << dp[n];
     return 0;
