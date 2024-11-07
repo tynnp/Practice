dcf48d6380b6b6bf453cc703300dd2a87f13c0b3
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-08 02:55:20 +0700

    Removing Digits

--- CSES/CSES Problem Set/Dynamic Programming/Removing Digits.cpp
+++ CSES/CSES Problem Set/Dynamic Programming/Removing Digits.cpp
@@ -1,34 +1,36 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
- 
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
 using namespace __gnu_pbds;
- 
+
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
- 
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
 const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
- 
-int n;
-vector<int> dp(MAXN, INT_MAX);
- 
+
+int n, dp[MAXN];
+
 signed main() {
     fastIO;
     cin >> n;
+    fill(dp, dp + MAXN, INT_MAX);
+
     dp[0] = 0;
-    
-    for (int i = 0; i <= n; i++) {
-        int k = i;
-        
-        while (k) {
-            dp[i] = min(dp[i], dp[i - k % 10] + 1);
-            k /= 10;
+    for (int i = 1; i <= n; i++) {
+        int d = i;
+
+        while (d) {
+            dp[i] = min(dp[i], dp[i - d % 10] + 1);
+            d /= 10;
         }
     }
     
