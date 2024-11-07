4c0f2eb5e3066cc50fdf186727109f453a18a7ed
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-08 03:11:06 +0700

    Grid Paths

--- CSES/CSES Problem Set/Dynamic Programming/Grid Paths.cpp
+++ CSES/CSES Problem Set/Dynamic Programming/Grid Paths.cpp
@@ -1,50 +1,48 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
- 
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
 using namespace __gnu_pbds;
- 
+
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
- 
-const int MAXN = 1e3 + 5;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e4 + 5;
 const int MOD = 1e9 + 7;
- 
-int n;
-char a[MAXN][MAXN];
-int dp[MAXN][MAXN];
- 
+
+int n, dp[MAXN][MAXN];
+char c[MAXN][MAXN];
+
 signed main() {
     fastIO;
     cin >> n;
-    
-    for (int i = 0; i < n; i++) 
+
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            cin >> a[i][j];
-        
+            cin >> c[i][j];
+
+    dp[0][0] = (c[0][0] == '*' ? 0 : 1);
+
     for (int i = 0; i < n; i++) {
-        if (a[i][0] == '*')
-            break;
-        dp[i][0] = 1;
+        for (int j = 0; j < n; j++) {
+            if (c[i][j] == '*')
+                continue;
+
+            if (i > 0)  
+                (dp[i][j] += dp[i-1][j]) %= MOD;
+
+            if (j > 0)
+                (dp[i][j] += dp[i][j-1]) %= MOD;
+        }
     }
-    
-    for (int i = 0; i < n; i++) {
-        if (a[0][i] == '*')
-            break;
-        dp[0][i] = 1;
-    }
-    
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < n; j++)
-            if (a[i][j] != '*')
-                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD;
-    }
-    
+
     cout << dp[n-1][n-1];
     return 0;
-}
+} 
