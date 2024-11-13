059b8f804ed190d6f133d2dd0ecb894b33614ad8
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-13 14:29:58 -0500

    Grid Paths

--- CSES/CSES Problem Set/Dynamic Programming/Grid Paths.cpp
+++ CSES/CSES Problem Set/Dynamic Programming/Grid Paths.cpp
@@ -14,35 +14,39 @@ using namespace __gnu_pbds;
 template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
-const int MAXN = 1e4 + 5;
+const int MAXN = 1000 + 5;
 const int MOD = 1e9 + 7;
 
-int n, dp[MAXN][MAXN];
-char c[MAXN][MAXN];
+int n, c[MAXN][MAXN];
+int dp[MAXN][MAXN];
 
 signed main() {
     fastIO;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> c[i][j];
-
-    dp[0][0] = (c[0][0] == '*' ? 0 : 1);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (c[i][j] == '*')
-                continue;
-
-            if (i > 0)  
-                (dp[i][j] += dp[i-1][j]) %= MOD;
-
-            if (j > 0)
-                (dp[i][j] += dp[i][j-1]) %= MOD;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            char x;
+            cin >> x;   
+            c[i][j] = x == '.';
         }
     }
 
-    cout << dp[n-1][n-1];
+    dp[1][1] = c[1][1];
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (!c[i][j]) 
+                continue;
+        
+            if (i > 1)
+                (dp[i][j] += dp[i-1][j]) %= MOD;
+            
+            if (j > 1) 
+                (dp[i][j] += dp[i][j-1]) %= MOD;
+        }
+    }
+    
+    cout << dp[n][n];
     return 0;
-} 
+}
