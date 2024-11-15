f2a8cb72268d587214cd89baabae4b8349ac5309
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-15 12:11:21 -0500

    Frog 1

--- ONLINE JUDGE/VNOJ/Other/atcoder_dp_a.cpp
+++ ONLINE JUDGE/VNOJ/Other/atcoder_dp_a.cpp
@@ -28,7 +28,7 @@ signed main() {
         cin >> h[i];
 
     dp[2] = abs(h[2] - h[1]);
-    
+
     for (int i = 3; i <= n; i++)
         dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]), dp[i-2] + abs(h[i] - h[i-2]));
     
