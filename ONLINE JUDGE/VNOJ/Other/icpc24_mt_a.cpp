f8d38801e0ea4b5773c597a7e500a20353d0fabe
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-16 18:41:08 +0700

    ICPC 2024 miền Trung - A: Thief

--- ONLINE JUDGE/VNOJ/Other/icpc24_mt_a.cpp
+++ ONLINE JUDGE/VNOJ/Other/icpc24_mt_a.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
@@ -10,47 +11,42 @@ using namespace __gnu_pbds;
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 1e5 + 5;
 const int MOD = 1e9 + 7;
 
-int N, W, H, sum;
-int w[105], c[105], dp[105][MAXN][2];
+int N, W, H;
+int w[105], v[105];
+int dp[MAXN][2];
 
 signed main() {
     fastIO;
     cin >> N >> W >> H;
 
-    for (int i = 1; i <= N; i++) {
-        cin >> w[i] >> c[i];
-        sum += c[i];
-    }
+    for (int i = 1; i <= N; i++)
+        cin >> w[i] >> v[i];
 
-    memset(dp, 0x3f, sizeof(dp));
-    dp[0][0][0] = 0;
+    fill(&dp[0][0], &dp[0][0] + 2*MAXN, 1e9 + 7);
+    dp[0][0] = dp[0][1] = 0;
 
     for (int i = 1; i <= N; i++) {
-        for (int j = 0; j <= sum; j++) {
-            dp[i][j][0] = dp[i-1][j][0];
-            dp[i][j][1] = dp[i-1][j][1];
+        for (int j = MAXN - 1; j >= v[i]; j--) {
+            dp[j][0] = min(dp[j][0], dp[j - v[i]][0] + w[i]);
+            dp[j][1] = min(dp[j][1], dp[j - v[i]][1] + w[i]);
 
-            if (j - c[i] >= 0) {
-                dp[i][j][0] = min(dp[i][j][0], dp[i-1][j - c[i]][0] + w[i]);
-                dp[i][j][1] = min(dp[i][j][1], dp[i-1][j - c[i]][1] + w[i]);
-            }
-
-            if (j - c[i] >= 0 && w[i] <= H) 
-                dp[i][j][1] = min(dp[i][j][1], dp[i-1][j - c[i]][0]);
+            if (w[i] <= H)
+                dp[j][1] = min(dp[j][1], dp[j - v[i]][0]);
         }
     }
-
-    for (int ans = sum; ans > 0; ans--) {
-        if (dp[N][ans][1] <= W || dp[N][ans][0] <= W) {
+    
+    for (int ans = MAXN - 1; ans > 0; ans--) {
+        if (dp[ans][0] <= W || dp[ans][1] <= W) {
             cout << ans;
             break;
         }
     }
-    
+
     return 0;
 }
