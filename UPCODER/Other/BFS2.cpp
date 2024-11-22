dce39c48b52e63b079d2e72ed108a8306b123df5
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-22 22:27:09 +0700

    BFS từ S đến T

--- UPCODER/Other/BFS2.cpp
+++ UPCODER/Other/BFS2.cpp
@@ -1,59 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <cstring>
-using namespace std;
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
-int n, m, s, t;
-vector<int> vt[1001];
-bool visited[1001];
-int res[1001];
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e5 + 5;
+const int MOD = 1e9 + 7;
+
+int N, M, S, T;
+vector<int> adj[MAXN];
+bool visited[MAXN];
+int parent[MAXN];
 
 void BFS(int u) {
     queue<int> q;
     q.push(u);
     visited[u] = true;
-    
+
     while (!q.empty()) {
         int v = q.front();
         q.pop();
 
-        for (int x : vt[v]) {
+        for (int x : adj[v]) {
             if (!visited[x]) {
                 q.push(x);
                 visited[x] = true;
-                res[x] = v; 
+                parent[x] = v;
             }
         }
     }
 }
 
-void printShortestPath() {
-    vector<int> path;
-    int current = t;
-    while (current != s) {
-        path.push_back(current);
-        current = res[current];
-    }
-    path.push_back(s);
-    
-    for (int i = path.size() - 1; i >= 0; --i) 
-        cout << path[i] << " ";
-}
+signed main() {
+    fastIO;
+    cin >> N >> M >> S >> T;
 
-int main() {
-    cin >> n >> m >> s >> t;
-    
-    for (int i = 0; i < m; i++) {
-        int x, y;
-        cin >> x >> y;
-        vt[x].push_back(y);
-        vt[y].push_back(x);
+    while (M--) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    memset(visited, false, sizeof(visited));
-    BFS(s);
-    printShortestPath();
+    BFS(S);
+    vector<int> res;
+
+    for (int v = T; v != 0; v = parent[v]) 
+        res.push_back(v);
+
+    for (int i = res.size(); i > 0; i--)
+        cout << res[i - 1] << ' ';
     
     return 0;
 }
