3e88dd3d6248d5dbf09e1daa797a45154d9974a4
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-24 12:27:19 +0700

    Concert Tickets

--- CSES/CSES Problem Set/Sorting and Searching/Concert Tickets.cpp
+++ CSES/CSES Problem Set/Sorting and Searching/Concert Tickets.cpp
@@ -1,50 +1,48 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
-const int MAXN = 1e6 + 5;
+const int MAXN = 2e5 + 5;
 const int MOD = 1e9 + 7;
 
 int n, m;
 multiset<int> s;
 
-void solve() {
-    cin >> n >> m;
-    
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-        s.insert(tmp);
-    }
-    
-    for (int i = 0; i < m; i++) {
-        int tmp;
-        cin >> tmp;
-        auto it = s.upper_bound(tmp);
-        
-        if (it == s.begin())
-            cout << -1 << endl;
-        else {
-            it--;
-            cout << *it << endl;
-            s.erase(it);
-        }
-    }
-}
-
 signed main() {
     fastIO;
-    int t;
-    //cin >> t;
-    t = 1;
+    cin >> n >> m;
+
+    while (n--) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+
+    while (m--) {
+        int x;
+        cin >> x;
+        auto it = s.upper_bound(x);
+
+        if (it == s.begin()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        cout << *(--it) << endl;
+        s.erase(it);
+    }
     
-    while (t--)
-        solve();
     return 0;
 }
