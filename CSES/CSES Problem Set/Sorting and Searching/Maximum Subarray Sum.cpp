3157458cca9c038424aa381229e1b979ae97dfe6
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-24 14:36:29 +0700

    Maximum Subarray Sum

--- CSES/CSES Problem Set/Sorting and Searching/Maximum Subarray Sum.cpp
+++ CSES/CSES Problem Set/Sorting and Searching/Maximum Subarray Sum.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
@@ -9,13 +10,14 @@ using namespace __gnu_pbds;
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-#define indexed_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 2e5 + 5;
 const int MOD = 1e9 + 7;
 
-int n, ans, sum;
+int n, sum, ans;
 int a[MAXN];
 
 signed main() {
@@ -24,13 +26,13 @@ signed main() {
 
     for (int i = 1; i <= n; i++)
         cin >> a[i];
-
+    
     ans = sum = a[1];
     for (int i = 2; i <= n; i++) {
         sum = max(a[i], sum + a[i]);
         ans = max(ans, sum);
     }
-    
+
     cout << ans;
     return 0;
 }
