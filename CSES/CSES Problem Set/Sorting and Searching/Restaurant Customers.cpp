4bb13692eab9b44d1d6e1fc380c79574b88c181d
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-24 15:08:41 +0700

    Restaurant Customers

--- CSES/CSES Problem Set/Sorting and Searching/Restaurant Customers.cpp
+++ CSES/CSES Problem Set/Sorting and Searching/Restaurant Customers.cpp
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
-const int MAXN = 1e6 + 5;
+const int MAXN = 2e5 + 5;
 const int MOD = 1e9 + 7;
 
-int n, cnt, ans;
-vector<pair<int, int>> v;
+struct Customer {
+    int time, tus;
+};
 
-void solve() {
-    cin >> n;
-    while (n--) {
-        int a, b;
-        cin >> a >> b;
-        v.push_back({a, 1});
-        v.push_back({b, -1});
-    }
-    
-    sort(v.begin(), v.end());
-    
-    for (auto x : v) {
-        cnt += x.second;
-        ans = max(ans, cnt);
-    }
-    
-    cout << ans;
-}
+int n, sum, ans;
+vector<Customer> a;
+multiset<int> res;
 
 signed main() {
     fastIO;
-    int t;
-    //cin >> t;
-    t = 1;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        a.push_back({x, 1});
+        a.push_back({y, -1});
+    }
     
-    while (t--)
-        solve();
+    sort(a.begin(), a.end(), [] (Customer &x, Customer &y) {
+        return x.time < y.time;
+    });
+
+    for (Customer x : a) {
+        sum += x.tus;
+        ans = max(ans, sum);
+    }
+    
+    cout << ans;
     return 0;
 }
