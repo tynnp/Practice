b3e5dcdce961e0fbb1661c314ad6eb2dcec224dc
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-26 13:44:58 +0700

    Bit Strings

--- CSES/CSES Problem Set/Introductory Problems/Bit Strings.cpp
+++ CSES/CSES Problem Set/Introductory Problems/Bit Strings.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
 
-int pow(int a, int b, int m) {
+int n;
+
+int Pow(int a, int b) {
     int res = 1;
-    a = a % m;
+    a %= MOD;
 
     while (b) {
-        if (b & 1) res = (a * res) % m;
-        a = (a * a) % m;
+        if (b & 1) 
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
         b >>= 1;
     }
 
@@ -26,8 +35,7 @@ int pow(int a, int b, int m) {
 
 signed main() {
     fastIO;
-    int n;
     cin >> n;
-    cout << pow(2, n, MOD);
+    cout << Pow(2, n);
     return 0;
 }
