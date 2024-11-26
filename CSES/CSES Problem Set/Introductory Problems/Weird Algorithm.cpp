c088a92d08570b8aadb14aceeb6c082d8452083d
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-26 11:18:22 +0700

    Weird Algorithm

--- CSES/CSES Problem Set/Introductory Problems/Weird Algorithm.cpp
+++ CSES/CSES Problem Set/Introductory Problems/Weird Algorithm.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
 
+int n;
+
 signed main() {
     fastIO;
-    int n;
     cin >> n;
 
-    while (n) {
-        cout << n << " ";
-        if (n == 1) break;
-        n = n & 1 ? 3*n + 1 : n/2;
+    while (n != 1) {
+        cout << n << ' ';
+        n = (n & 1) ? (3*n + 1) : (n >> 1);
     }
-    
+
+    cout << n;
     return 0;
 }
