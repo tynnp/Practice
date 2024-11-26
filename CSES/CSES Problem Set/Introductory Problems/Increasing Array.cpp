8273ead3d054bc4b17a7193bd5de582bdc2aec64
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-11-26 13:09:32 +0700

    Increasing Array

--- CSES/CSES Problem Set/Introductory Problems/Increasing Array.cpp
+++ CSES/CSES Problem Set/Introductory Problems/Increasing Array.cpp
@@ -1,31 +1,38 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int long long
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 2e5 + 5;
 const int MOD = 1e9 + 7;
 
-int n, k, ans;
+int n, ans;
 int a[MAXN];
 
 signed main() {
     fastIO;
     cin >> n;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 1; i <= n; i++)
         cin >> a[i];
 
-    for (int i = 1; i < n; i++) {
-        k = max(0LL, a[i-1] - a[i]);
-        ans += k;
-        a[i] += k;
+    for (int i = 2; i <= n; i++) {
+        if (a[i-1] > a[i])
+            ans += a[i-1] - a[i];
+        a[i] = max(a[i], a[i-1]);
     }
-    
+
     cout << ans;
     return 0;
 }
