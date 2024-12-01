3c06fa5241b9e25016eee227c4ea9871f22ceabd
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-01 15:02:19 +0700

    PARK - Trông xe (OLP chuyên 2010)

---
+++ UPCODER/Other/PARK.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int n, t, ans;
+int a[MAXN], cnt;
+map<int, int> m;
+
+signed main() {
+    fastIO;
+    cin >> n;
+
+    while (n--) {
+        cin >> t;
+        if (!m[t]) 
+            a[cnt++] = t;
+        m[t]++;
+    }
+
+    for (int i = 0; i < cnt; i++) 
+        if (m[a[i]] <= 5) ans += 100;
+        else ans += 100 + (m[a[i]] - 5);
+
+    cout << ans;
+    return 0;
+}
