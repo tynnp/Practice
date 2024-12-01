ee206dcab4c0818fda2bd11ffd4eb83ea2dea378
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-01 13:13:51 +0700

    Factorial

---
+++ UPCODER/Other/Factorial.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+signed main() {
+    fastIO;
+    int n, ans = 1;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        ans *= i;
+
+        while (ans % 10 == 0) 
+            ans /= 10;
+            
+        ans %= 100000;
+    }
+
+    cout << ans % 10;
+    return 0;
+}
