5d7a35b749c4ba2a1d7f39ab66f9d7da81e8fe88
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-02 13:37:04 +0700

    Username

---
+++ UPCODER/Other/USERNAME.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int n, cnt;
+string s;
+map<string, int> m;
+
+signed main() {
+    fastIO;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> s;
+
+        if (m[s]) {
+            cout << i << ' ';
+            cnt++;
+        } else {
+            m[s]++;
+        }
+    }
+    
+    if (!cnt) cout << -1;
+    return 0;
+}
