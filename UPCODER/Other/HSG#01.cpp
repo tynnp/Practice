dbfda7ebc9ada28b91d85a7d77a2c1549f8ac0a4
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-03 22:51:41 +0700

    Số dư thừa

---
+++ UPCODER/Other/HSG#01.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int sum(int n) {
+    int res = 0;
+    for (int i = 1; i <= n/2; i++)
+        if (n % i == 0)
+            res += i;
+    return res;
+}
+
+signed main() {
+    fastIO;
+    int n;
+    cin >> n;
+    
+    n++;
+    while (true) {
+        int s = sum(n);
+
+        if (s > n) {
+            cout << n;
+            break;
+        }
+
+        n++;
+    }
+    
+    return 0;
+}
