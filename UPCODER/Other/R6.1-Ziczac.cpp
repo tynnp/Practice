1ba7dc6775b58ce95d608a004d3a437298f78636
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-04 23:38:39 +0700

    Ziczac

---
+++ UPCODER/Other/R6.1-Ziczac.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int x, y, n;
+int odd, even;
+
+signed main() {
+    fastIO;
+    cin >> x >> y >> n;
+
+    odd = (n + 1) >> 1;
+    even = n >> 1;
+
+    x += odd - even;
+    y += odd; 
+
+    cout << x << ' ' << y;
+    return 0;
+}
