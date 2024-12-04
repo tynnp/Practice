24adbbcb2c152708c800cb4c96c90b9f399c86fb
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-04 21:48:57 +0700

    Tìm số khác biệt

---
+++ UPCODER/Other/MINIGAME12.1-DIFFNUM.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define int long long
+#define inp freopen("file.inp", "r", stdin)
+#define out freopen("file.out", "w", stdout)
+#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int n, chan, le;
+int ansChan, ansLe;
+
+signed main() {
+    fastIO;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+
+        if (x & 1) {
+            le++;
+            ansLe = i;
+
+        } else {
+            chan++;
+            ansChan = i;
+        }
+    }
+    
+    if (chan == 1)
+        cout << ansChan;
+    else
+        cout << ansLe;
+
+    return 0;
+}
