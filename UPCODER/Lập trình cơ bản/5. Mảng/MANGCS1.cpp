10079823568215869fd85b2ae87e0559363cbe58
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-12-20 19:07:43 +0700

    Update MANGCS1.cpp

--- UPCODER/Lập trình cơ bản/5. Mảng/MANGCS1.cpp
+++ UPCODER/Lập trình cơ bản/5. Mảng/MANGCS1.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int index, value;
-    cin >> index >> value;
-
-    int arr[1000], n = 0;
-    while (cin >> arr[n]) n++;
-
+    int x, v;
+    int n = 0, a[100];
+    
+    cin >> x >> v;
+    
+    while (cin >> a[n]) 
+        n++;
+        
     for (int i = 0; i < n; i++) {
-        if (i == index) cout << value << " ";
-        cout << arr[i] << " ";
+        if (i == x) 
+            cout << v << ' ';
+        cout << a[i] << ' ';
     }
-
-    if (index == n) cout << value;
+    
+    if (x == n)
+        cout << v;
+    
     return 0;
 }
